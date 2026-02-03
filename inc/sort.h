363c1556af54bde5f020de93890a037583be1937
vledbress <vledbress388@gmail.com>
2026-02-03 19:40:13 +0100

    .

--- inc/sort.h
+++ inc/sort.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sort_utils.h                                       :+:      :+:    :+:   */
+/*   sort.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vborysov <vborysov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/30 01:19:30 by vborysov          #+#    #+#             */
-/*   Updated: 2026/02/03 19:23:08 by vborysov         ###   ########.fr       */
+/*   Updated: 2026/02/03 19:40:04 by vborysov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SORT_UTILS_H
-# define SORT_UTILS_H
+#ifndef SORT_H
+# define SORT_H
 # include "stack.h"
 
 void	ft_sort(t_stack	*stack_a, t_stack	*stack_b);
