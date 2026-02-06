bff926f6add98be2fd63fde1591560861106b222
vblxssv <vledbress388@gmail.com>
2026-02-07 00:08:21 +0100

    .

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: vborysov <vborysov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 17:50:42 by vborysov          #+#    #+#             */
-/*   Updated: 2026/02/06 21:13:27 by vborysov         ###   ########.fr       */
+/*   Updated: 2026/02/07 00:07:21 by vborysov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include "parse.h"
 #include <stdio.h>
 #include "sort.h"
+#include "binary.h"
 
 
 int main(int argc, char **argv)
@@ -23,26 +24,20 @@ int main(int argc, char **argv)
 		exit(1);
 	int	len;
 	long	*array;
-
-	ft_parse_args(argc, argv, &array, &len);
+	int	*copy_sorted;
 	
+	ft_parse_args(argc, argv, &array, &len);
+	copy_sorted = malloc(sizeof(int) * len);
+	for(int i = 0; i < len; ++i)
+		copy_sorted[i] = (int)array[i];
+	ft_quick_sort(copy_sorted, 0, len - 1);
 	t_stack	*a = NULL;
 	t_stack	*b = NULL;
 	for(int i = 0; i < len; ++i)
-		ft_push_bottom(&a, ft_new_node(array[i]));
-
-	
-	for (t_stack	*curr = a; curr; curr = curr->next)
-		printf("%d ",curr->data);
-	printf("\n");
-	
+		ft_push_bottom(&a, ft_new_node(ft_binary_search(copy_sorted, len, (int)array[i])));
 	ft_sort(&a, &b);
-	
-	for (t_stack	*curr = a; curr; curr = curr->next)
-		printf("%d ",curr->data);
-	
-
 	ft_destroy_stack(&a);
 	ft_destroy_stack(&b);
 	free(array);
+	free(copy_sorted);
 }
