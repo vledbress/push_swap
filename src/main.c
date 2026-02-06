c220b4c1c6c810eccfb7b99dace7e54b64ab9421
vledbress <vledbress388@gmail.com>
2026-02-06 15:55:42 +0100

    .

--- src/main.c
+++ src/main.c
@@ -6,10 +6,9 @@
 /*   By: vborysov <vborysov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 17:50:42 by vborysov          #+#    #+#             */
-/*   Updated: 2026/02/06 15:51:16 by vborysov         ###   ########.fr       */
+/*   Updated: 2026/02/06 15:52:57 by vborysov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "../libft/libft.h"
 #include "error.h"
@@ -17,14 +16,12 @@
 
 int main(int argc, char **argv)
 {
-	if (argc < 2)
-		ft_error();
+	if (argc == 1)
+		exit(1);
 	int	len;
 	long	*array;
 	
 	ft_parse_args(argc, argv, &array, &len);
-	
-
 	
 	free(array);
 }
