4c51747f82ab81fb38c5abb20be09826af33be0c
vblxssv <vledbress388@gmail.com>
2026-02-07 17:46:07 +0100

    .

--- src/sort_move_b.c
+++ src/sort_move_b.c
@@ -6,7 +6,7 @@
 /*   By: vborysov <vborysov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 19:10:57 by vborysov          #+#    #+#             */
-/*   Updated: 2026/02/07 16:32:16 by vborysov         ###   ########.fr       */
+/*   Updated: 2026/02/07 16:39:35 by vborysov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,6 @@ static int	ft_calculate_chunk(int total_size)
 		return (15);
 	return (32);
 }
-
-// static int ft_calculate_chunk(int total_size)
-// {
-// 	int	K;
-
-// 	if (total_size <= 100)
-// 		K = 6;
-// 	else
-// 		K = 12;
-// 	return (total_size / K + 1);
-// }
-
-// static int ft_calculate_chunk(int total_size)
-// {
-//     if (total_size <= 100)
-//         return total_size / 6 + 1;
-//     if (total_size <= 500)
-//         return total_size / 11 + 1;
-//     return total_size / 20 + 1;
-// }
 
 void	ft_move_chunks_b(t_stack	**a, t_stack	**b)
 {
@@ -65,57 +45,3 @@ void	ft_move_chunks_b(t_stack	**a, t_stack	**b)
 			ft_rotate(a, "ra");
 	}
 }
-
-// void	ft_move_chunks_b(t_stack **a, t_stack **b)
-// {
-// 	int		i;
-// 	int		range;
-// 	int		pos;
-// 	t_stack	*tmp;
-// 	int		size;
-
-// 	if (!a || !b)
-// 		return ;
-
-// 	i = 0;
-// 	range = ft_calculate_chunk(ft_stack_size(*a));
-
-// 	while (ft_stack_size(*a) > 0)
-// 	{
-// 		// ищем позицию ближайшего элемента в текущем чанке
-// 		pos = 0;
-// 		tmp = *a;
-// 		size = ft_stack_size(*a);
-// 		while (tmp && tmp->data > i + range)
-// 		{
-// 			tmp = tmp->next;
-// 			pos++;
-// 		}
-
-// 		// умная ротация A
-// 		if (pos <= size / 2)
-// 		{
-// 			while (pos--)
-// 				ft_rotate(a, "ra");
-// 		}
-// 		else
-// 		{
-// 			pos = size - pos;
-// 			while (pos--)
-// 				ft_rev_rotate(a, "rra");
-// 		}
-
-// 		// теперь верхний элемент точно в текущем чанке
-// 		if ((*a)->data <= i)
-// 		{
-// 			ft_push(b, a, "pb");
-// 			ft_rotate(b, "rb");
-// 			i++;
-// 		}
-// 		else if ((*a)->data <= i + range)
-// 		{
-// 			ft_push(b, a, "pb");
-// 			i++;
-// 		}
-// 	}
-// }
